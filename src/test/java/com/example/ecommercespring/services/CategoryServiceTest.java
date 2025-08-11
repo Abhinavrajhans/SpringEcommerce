2e7b733589af549885447f0ec09ab1be9182e02a
abhinavrajhans <abhinav.rajhans1@gmail.com>
2025-08-11 15:33:02 +0530

    added eureka client

---
+++ src/test/java/com/example/ecommercespring/services/CategoryServiceTest.java
@@ -0,0 +1,16 @@
+package com.example.ecommercespring.services;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class) // this annotation is used to enable Mokito for the test class with Junit.
+public class CategoryServiceTest {
+
+    @Test
+    @DisplayName("should return all the categories successfully")
+    void getAllCategories_shouldReturnAllCategories(){
+
+    }
+}
